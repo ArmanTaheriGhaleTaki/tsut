25ed7cd507732197f67b28356a8e6d7502d34910
amirrezaalavi <alavi2004@outlook.com>
2024-04-30 23:27:26 +0330

    fixed split_audio function

--- scripts/twitter_space_downloader.py
+++ scripts/twitter_space_downloader.py
@@ -1,11 +1,10 @@
 import subprocess
 import glob
 import ffmpeg
-import math
 import os
 
 def download_audio(space_link, cookie_file):
-    subprocess.run(["twspace_dl", "-i", space_link,"-c" ,cookie_file ,"-o","%(title)s;%(start_date)s;%(creator_name)s;%(creator_screen_name)s;" ])
+    subprocess.run(["twspace_dl", "-i", space_link,"-c" ,cookie_file ,"-o","%(title)s;%(start_date)s;%(creator_name)s;%(creator_screen_name)s;" ]) # get the data from cookie file
     text= glob.glob('*.m4a')
     with open('output.txt', 'w') as file:
         for i in text:
@@ -16,31 +15,12 @@ def download_audio(space_link, cookie_file):
             split_audio(newname)
 
 def split_audio(newname): # Added python-native ffmpeg library instead of calling it using executable
+    input_audio = ffmpeg.input(newname)
+    output_segments = ffmpeg.output(input_audio, f"{newname}_%01d.m4a", format='segment', segment_time=3600, c='copy')     # define the output segments
+    ffmpeg.run(output_segments) # execute the ffmpeg command
 
 
-    probe = ffmpeg.probe(input_file)
-    video_info = next(s for s in probe['streams'] if s['codec_type'] == 'video')
-    duration = float(video_info['duration'])
-
-    # Calculate the number of segments
-    num_segments = math.ceil(duration / segment_length)
-
-    # Split the video
-    for i in range(num_segments):
-        start_time = i * segment_length
-        end_time = start_time + segment_length
-        output_file = f"{newname}_{str(i+1).zfill(3)}.m4a"
-
-        # Use ffmpeg-python to split the video
-        (
-            ffmpeg
-            .input(input_file)
-            .output(output_file, ss=start_time, t=segment_length, c='copy')
-            .run()
-        )
-
-
-#   subprocess.run(
+#   subprocess.run( # not deleted to restore in case of compatibility issues
 #       [
 
 
