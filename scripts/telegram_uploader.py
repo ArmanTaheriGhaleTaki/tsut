483d6e2260a4a888a5ecad8c54d2bd8da274d7bf
amirrezaalavi <alavi2004@outlook.com>
2024-04-30 23:28:08 +0330

    use the official telegram api instead of curl

--- scripts/telegram_uploader.py
+++ scripts/telegram_uploader.py
@@ -1,24 +1,30 @@
 import os
 import subprocess
+from telegram import Bot
+
 def upload_to_telegram(bot_token, channel_id,directory):
-    with open('output.txt', 'r') as desc:
+    bot = Bot(token=bot_token) # initialize bot
+    with open('output.txt', 'r') as desc: # read the desc
         message = desc.read()
-    for file in os.listdir(directory):
+    for file in os.listdir(directory): # open file
         if file.endswith('m4a'):
-           print(file)
-           filepath = os.path.join(directory, file)
-           subprocess.run(
-              [
-               "curl",
-               "-s",
-               "-X",
-               "POST",
-               f"https://api.telegram.org/bot{bot_token}/sendDocument",
-               "-F",
-               f"chat_id={channel_id}",
-               "-F",
-               f"document=@{file}",
-               "-F",               
-               f"caption={message}",
-              ]
-    )
+            print(file)
+            filepath = os.path.join(directory, file) # store the full path
+            with open(filepath, 'rb') as video_file: # open the file
+                bot.send_video(chat_id=channel_id, video=video_file, supports_streaming=True)
+
+           # subprocess.run(
+           #    [
+           #     "curl",
+           #     "-s",
+           #     "-X",
+           #     "POST",
+           #     f"https://api.telegram.org/bot{bot_token}/sendDocument",
+           #     "-F",
+           #     f"chat_id={channel_id}",
+           #     "-F",
+           #     f"document=@{file}",
+           #     "-F",
+           #     f"caption={message}",
+           #    ]
+            #)
